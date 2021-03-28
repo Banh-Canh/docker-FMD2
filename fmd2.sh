1fcbeb1c2a5f1607812737b39bbd4a10980b984d
Victor <tkvictor-hang@outlook.fr>
2021-03-28 14:35:17 +0000

    Update fmd2.sh

--- fmd2.sh
+++ fmd2.sh
@@ -2,4 +2,5 @@
 Xvfb :1 -screen 0 1600x900x24 &
 x11vnc -display :1 -noipv6 -reopen -forever -repeat -loop -rfbport 5900 -noxdamage &
 websockify -D --web=/usr/share/novnc/ 6080 localhost:5900 &
+wineserver -k
 wine /app/FMD2/fmd.exe
